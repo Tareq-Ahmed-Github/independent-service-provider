0a855c557bf3f0e0e6436288327bfcf645eff7de
unknown <tareqrobin57@gmail.com>
2022-05-09 22:57:56 +0600

    things done before authentication. next step will be authentication

--- src/Components/Js/Services.js
+++ src/Components/Js/Services.js
@@ -1,52 +1,57 @@
 import React from 'react';
-import { Card, CardGroup, Row } from 'react-bootstrap';
+import { Button, Card, CardGroup, Row } from 'react-bootstrap';
 
 const Services = () => {
     return (
         <div id='services' className='flex'>
-            <h2>Our Services</h2>
-            <CardGroup>
+            <h2 className='my-5'>Our Services</h2>
+            <CardGroup className='h-75 w-75 mx-auto'>
                 <Row md={2}>
                     <Card>
                         <Card.Img variant="top" src="https://i.ibb.co/qFZjJx7/young-fitness-man-studio.jpg" />
                         <Card.Body>
-                            <Card.Title>Card title</Card.Title>
+                            <Card.Title>name</Card.Title>
                             <Card.Text>
                                 This is a wider card with supporting text below as a natural lead-in to
                                 additional content. This content is a little bit longer.
                             </Card.Text>
+                            <Button variant='primary'>buy</Button>
                         </Card.Body>
                     </Card>
                     <Card>
                         <Card.Img variant="top" src="https://i.ibb.co/qFZjJx7/young-fitness-man-studio.jpg" />
                         <Card.Body>
-                            <Card.Title>Card title</Card.Title>
+                            <Card.Title>name</Card.Title>
                             <Card.Text>
                                 This card has supporting text below as a natural lead-in to additional
                                 content.{' '}
                             </Card.Text>
+                            <Card.Text>price</Card.Text>
+                            <Button variant='primary'>buy</Button>
                         </Card.Body>
                     </Card>
                     <Card>
                         <Card.Img variant="top" src="https://i.ibb.co/qFZjJx7/young-fitness-man-studio.jpg" />
                         <Card.Body>
-                            <Card.Title>Card title</Card.Title>
+                            <Card.Title>name</Card.Title>
                             <Card.Text>
                                 This is a wider card with supporting text below as a natural lead-in to
                                 additional content. This card has even longer content than the first to
                                 show that equal height action.
                             </Card.Text>
+                            <Button variant='primary'>buy</Button>
                         </Card.Body>
                     </Card>
                     <Card>
                         <Card.Img variant="top" src="https://i.ibb.co/qFZjJx7/young-fitness-man-studio.jpg" />
                         <Card.Body>
-                            <Card.Title>Card title</Card.Title>
+                            <Card.Title>name</Card.Title>
                             <Card.Text>
                                 This is a wider card with supporting text below as a natural lead-in to
                                 additional content. This card has even longer content than the first to
                                 show that equal height action.
                             </Card.Text>
+                            <Button variant='primary'>buy</Button>
                         </Card.Body>
                     </Card>
                 </Row>
--- src/Components/Js/Shared/Header.js
+++ src/Components/Js/Shared/Header.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/services">Services</Link>
-            </nav>
+            <Navbar sticky='top' bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Fitness Guru</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#services">Services</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
         </div>
     );
 };
